8a7ba20586bf9d5f54531a5a447a4b87bb1ef6c0
Damiana-Arangio <arangio.damiana@gmail.com>
2026-01-02 19:24:28 +0100

    feat(searchbar): implementata UI e logica base

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,62 @@
 
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
 
 function Searchbar() {
+
+    /**********
+        HOOK
+    ***********/
+
+    const [isOpen, setIsOpen] = useState(false);    // Stato che gestisce l'apertura/chiusura della searchbar
+    const [query, setQuery] = useState("")          // Stato che gestisce il valore dell'input della searchbar
+
+    // Debug
+    console.log(isOpen);
+    console.log(query); 
+
     return (
-            <h1>Searchbar</h1>
+
+        <form onSubmit={handleSubmit} className= {isOpen && "searchbar"}>
+
+            {/* Icona ricerca */}
+            <FontAwesomeIcon icon={faMagnifyingGlass} onClick={openSearchbar} className="iconSearch" />
+
+            {/* Campo di input visibile solo quando la searchbar è aperta */}
+            {isOpen && (
+                <input 
+                    type="text" 
+                    value={query}
+                    placeholder="Titolo, persone, generi"
+                    onChange={handleChange}
+                    className="input"
+                />
+            )}
+            
+        </form>
     )
+
+
+    /**************
+        FUNZIONI
+    **************/
+
+    // Funzione che cambia lo stato della searchbar (chiusa/aperta)
+    function openSearchbar() {
+        setIsOpen(isOpen => !isOpen);
+    }
+
+    // Funzione che blocca l'invio automatico del form
+    function handleSubmit(e) {
+        e.preventDefault();     // Blocca invio form
+        setQuery("");
+    }
+
+    // Funziona che aggiorna il valore dell'input di ricerca
+    function handleChange(e) {
+        setQuery(e.target.value)
+    }
 }
 
 export default Searchbar;
