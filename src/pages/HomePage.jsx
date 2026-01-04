a4c50d8bf5f5f3ad04b7d2e7f5a69e3a498b8831
Damiana-Arangio <arangio.damiana@gmail.com>
2026-01-04 01:15:42 +0100

    refactor(home: centralizzate funzioni chiamate API e rendering sezioni

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,26 +4,41 @@ import axios from 'axios';
 
 function HomePage() {
 
-    /**************
-        COSTANTI
-    ***************/
-    const API_URL = import.meta.env.VITE_API_URL;
-    const API_KEY = import.meta.env.VITE_API_KEY;
-
     /***********
         HOOK
     ************/
-    const [FilmDiTendenza, setFilmDiTendenza] = useState([]);
+
+    // Stato
+    const [filmDiTendenza, setFilmDiTendenza] = useState([]);
     const [tiAppassioneranno, setTiappassioneranno] = useState([]);
     const [serieInOndaOggi, setSerieInOndaOggi] = useState([]);
     const [tesoriPerTe, setTesoriPerTe] = useState([]);
     
+    // Effetto
     useEffect( () => {
-        fetchFilmDiTendenza();
-        fetchTiAppassioneranno();
-        fetchSerieInOndaOggi();
-        fetchTesoriPerTe();
+        fetchSection("/trending/movie/day", setFilmDiTendenza);
+        fetchSection("/tv/top_rated", setTiappassioneranno);
+        fetchSection("/tv/airing_today", setSerieInOndaOggi);
+        fetchSection("/movie/top_rated", setTesoriPerTe);
     }, [] );
+
+
+    /**************
+        COSTANTI
+    ***************/
+
+    // Sezioni home
+    const homeSections = [
+        { id: 1, title: "Film di tendenza", items: filmDiTendenza}, 
+        { id: 2, title: "Pensiamo ti appassioneranno", items: tiAppassioneranno }, 
+        { id: 3, title: "Serie tv in onda oggi", items: serieInOndaOggi }, 
+        { id: 4, title: "Tesori per te", items: tesoriPerTe }, 
+    ]
+
+    // Chiamate API
+    const API_URL = import.meta.env.VITE_API_URL;
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
 
     /**************
         RENDERING
@@ -31,90 +46,27 @@ function HomePage() {
     return (
         <>
             
-            <MediaSection
-                title="Film di tendenza"
-                items={FilmDiTendenza}
-            />
-
-            <MediaSection
-                title="Pensiamo ti appassioneranno"
-                items={tiAppassioneranno}
-            />
-
-            <MediaSection
-                title="Serie tv in onda oggi"
-                items={serieInOndaOggi}
-            />
-
-            <MediaSection
-                title="Tesori per te"
-                items={tesoriPerTe}
-            />
+            {homeSections.map( section => (
+                <MediaSection
+                    key = {section.id}
+                    title={section.title}
+                    items={section.items}
+                />
+            ))}
         </>
     )
 
     /**************
         FUNZIONI
     ***************/
-    function fetchFilmDiTendenza() {
+    function fetchSection(endpoint, setState) {
 
-        const url = `${API_URL}/trending/movie/day?api_key=${API_KEY}&language=it-IT`;
+        const url = `${API_URL}${endpoint}?api_key=${API_KEY}&language=it-IT&page=1`;
 
         axios.get(url)
-            .then(res => {
-                console.log("API film di tendenza", res.data.results);
-
-                setFilmDiTendenza(res.data.results);
-            })
-            .catch(err => {
-                console.error("Errore chiamata API", err);
-            });
+            .then(res => setState(res.data.results))
+            .catch(err => console.error("Errore chiamata API", err))
     };
-
-    function fetchTiAppassioneranno() {
-
-        const url = `${API_URL}/tv/top_rated?api_key=${API_KEY}&language=it-IT&page=1`;
-
-        axios.get(url)
-            .then(res => {
-                console.log("API TI APPASSIONERANNO:", res.data.results);
-
-                setTiappassioneranno(res.data.results);
-            })
-            .catch(err => {
-                console.error("Errore chiamata API", err);
-            });
-    }
-
-    function fetchSerieInOndaOggi() {
-
-        const url = `${API_URL}/tv/airing_today?api_key=${API_KEY}&language=it-IT&page=1`;
-
-        axios.get(url)
-            .then(res => {
-                console.log("RISPOSTA API serie tv in onda oggi:", res.data.results);
-
-                setSerieInOndaOggi(res.data.results);
-            })
-            .catch(err => {
-                console.error("Errore chiamata API", err);
-            });
-    }
-
-    function fetchTesoriPerTe() {
-
-        const url = `${API_URL}/movie/top_rated?api_key=${API_KEY}&language=it-IT&page=1`;
-
-        axios.get(url)
-            .then(res => {
-                console.log("RISPOSTA API tesori per te:", res.data.results);
-
-                setTesoriPerTe(res.data.results);
-            })
-            .catch(err => {
-                console.error("Errore chiamata API", err);
-            });
-    }
 
 }
 
