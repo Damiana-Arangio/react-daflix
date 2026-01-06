6c4022fde28644e62eb544304e05108e54004120
Damiana-Arangio <arangio.damiana@gmail.com>
2026-01-06 21:10:29 +0100

    feat(Hero): perfezionato layout  e aggiunta panoramica film

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,17 @@
 function Hero(props) {
 
-    const { title, poster } = props;
+    const { title, poster, overview } = props;
     const basePathImage = "https://image.tmdb.org/t/p/original";
 
     return(
 
-        
         <section className="heroSection">
 
             {/* Titolo Hero + logo */}
             <div className="containerLogoTitle">
-                <img src="/logo.png" alt="logo" className="logo" />
+                <img src="/logo.png" alt="logo" className="heroLogo" />
                 <h1 className="heroTitle"> {title.toUpperCase()} </h1>
+                <p> {overview} </p>
             </div>
 
 
