055e43ecf6a0ca24f7e3b92d11264b65174be59a
Damiana-Arangio <arangio.damiana@gmail.com>
2026-01-07 23:30:16 +0100

    feat(mylist): struttura iniziale pagina con rendering condizionale

--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -1,8 +1,36 @@
-
+import MediaGrid from '../components/MediaGrid';
 
 function MyListPage() {
+
+    // Debug
+    const myList = [];
+
     return (
-        <h1>MyListPage</h1>
+        <>
+            <div className='myListPage'>
+
+                {/* Titolo pagina */}
+                <h1>La mia lista</h1>
+
+                {
+                    myList.length === 0 ? 
+                        (
+                            <div className='containerListaVuota'>
+                                <p className='testoListaVuota'>Non hai aggiunto nessun titolo alla tua lista</p>
+                            </div>
+                        ) 
+                    : 
+                        (
+                            <div className='containerListaPiena'>
+                                <p>contenuti lista</p>
+                                {/* <MediaGrid/> */}
+                            </div> 
+                        )
+                }
+                
+            </div>
+
+        </>
     )
 }
 
